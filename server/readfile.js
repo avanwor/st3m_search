6922cc2d9dc8c4310252699a6723437197f9d89e
Andrew Van Wormer <a.vwormer@gmail.com>
2019-07-16 11:33:44 -0700

    Add comments for setting up autocorrect function

--- server/readfile.js
+++ server/readfile.js
@@ -4,9 +4,6 @@ const pullWords = () => {
     fs.readFile('./server/unix_words', function(err, data) {
         if(err) throw err;
         let array = data.toString().split("\n");
-        for(let i = 0; i < 5; i++) {
-            console.log(array[i]);
-        }
     });
 }
 
--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const pullWords = require('./readfile.js')
 
 const app = express();
 const port = 3008;
 
 app.use(express.static(__dirname + './../client/dist'));
+
+//Pull words from a dictionary file into memory
+let dictionary = pullWords.pullWords()
+
+//create post listener, and send array of possible correct user input to bst dictionary. 
+    //I like the idea of running cat cet cit cot cut on a multithreaded approach to search the array. Maybe run each on a promise?
 
 app.listen(port,() => console.log('serving at port ' + port));
